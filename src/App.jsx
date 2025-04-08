c26d231d47991b2828267010f1d35dc656a30c91
aniket shaoke <aniketshaoke33@gmail.com>
2025-04-09 03:54:42 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <HeroSection />
+      <Footer />
     </>
   );
 }
 
 export default App;
- 
